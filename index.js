eb4a9a248b0b5cd946758a4da69feff86563d725
Fantros <krostotr@gmail.com>
2022-12-08 05:22:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-import { readFileSync } from 'fs';
+var fs = require('fs');
 
-export default class templateEngine {
-    constructor(dir) {
-        this.dir = dir;
-    }
-
-    render(file, value) {
-        let data = readFileSync(`${this.dir}/${file}.html`, 'utf8');
-        if (value)
-            Object.keys(value).forEach((key) => data = data.replace(`{{ replace-${key} }}`, () => value[key])
-            );
-        return data;
-    }
+function templateEngine (dir) {
+    this.dir = dir;
 }
+  
+templateEngine.prototype.render = function (file, value) {
+    let data = fs.readFileSync(`${this.dir}/${file}.html`, 'utf8');
+    if (value) {
+        Object.keys(value).forEach(function(key) {
+            data = data.replace(`{{ replace-${key} }}`, () => value[key]);
+        });
+    }
+    return data;
+}
+
+module.exports = templateEngine;
