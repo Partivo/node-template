b8f1366d1a8d63106082330962b83ba047d788f1
Fantros <krostotr@gmail.com>
2022-12-28 20:08:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var fs = require('fs');
 
-function templateEngine (dir) {
+function templateEngine(dir) {
     this.dir = dir;
 }
   
-templateEngine.prototype.render = function (file, value) {
+templateEngine.prototype.renderFile = function (file, value) {
     let data = fs.readFileSync(`${this.dir}/${file}.html`, 'utf8');
+    if (value) {
+        Object.keys(value).forEach(function(key) {
+            data = data.replace(`{{ replace-${key} }}`, () => value[key]);
+        });
+    }
+    return data;
+}
+
+templateEngine.prototype.render = function (data, value) {
     if (value) {
         Object.keys(value).forEach(function(key) {
             data = data.replace(`{{ replace-${key} }}`, () => value[key]);
