4380edaa20283038284683b291f76d0c129df439
Fantros <krostotr@gmail.com>
2023-01-17 15:09:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 var fs = require('fs');
 
-function template() {}
-
-template.renderFile = function (file, data) {
-    let str = fs.readFileSync(file, 'utf8');
-    if (data) {
-        Object.keys(data).forEach(function(key) {
-            str = str.replace(`{{ ${key} }}`, () => data[key]);
-        });
-    }
-    return str;
+module.exports = {
+	renderFile: function (file, data) {
+		let str = fs.readFileSync(file, 'utf8');
+		if (data) {
+			Object.keys(data).forEach(function(key) {
+				str = str.replace(`{{ ${key} }}`, () => data[key]);
+			});
+		}
+		return str;
+	},
+	
+	render: function (str, data) {
+		if (data) {
+			Object.keys(data).forEach(function(key) {
+				str = str.replace(`{{ ${key} }}`, () => data[key]);
+			});
+		}
+		return str;
+	}
 }
-
-template.render = function (str, data) {
-    if (data) {
-        Object.keys(data).forEach(function(key) {
-            str = str.replace(`{{ ${key} }}`, () => data[key]);
-        });
-    }
-    return str;
-}
-
-module.exports = template;
