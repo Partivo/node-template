8dc752f6223d7d847d328e538930a0fff56cf049
Fantros <krostotr@gmail.com>
2023-01-17 15:14:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 module.exports = {
-	renderFile: function (file, data) {
+	renderFile: function(file, data) {
 		let str = fs.readFileSync(file, 'utf8');
 		if (data) {
 			Object.keys(data).forEach(function(key) {
@@ -11,7 +11,7 @@ module.exports = {
 		return str;
 	},
 	
-	render: function (str, data) {
+	render: function(str, data) {
 		if (data) {
 			Object.keys(data).forEach(function(key) {
 				str = str.replace(`{{ ${key} }}`, () => data[key]);
